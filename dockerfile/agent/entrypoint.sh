68ff8ea7818add84779c1c5c060c8160960ffe54
Pablo Garrido <pablogs9@gmail.com>
2020-03-09 10:41:06 +0100

    Update esntrypoints and rqt

--- dockerfile/agent/entrypoint.sh
+++ dockerfile/agent/entrypoint.sh
@@ -4,4 +4,4 @@ set -e
 source "/opt/ros/$ROS_DISTRO/setup.bash"
 source "/uros_ws/install/setup.bash"
 
-ros2 run micro_ros_agent micro_ros_agent serial --dev $SERIAL_DEV
+ros2 run micro_ros_agent micro_ros_agent serial --dev /dev/serial/by-id/usb-ZEPHYR_Zephyr_ST_Discovery_4E345008004C0049-if00 

17f46e53de0c4968459b2f781cc86ec07b91444d
Pablo Garrido <pablogs9@gmail.com>
2020-03-09 12:37:32 +0100

    Update entrypoint

--- dockerfile/agent/entrypoint.sh
+++ dockerfile/agent/entrypoint.sh
@@ -4,4 +4,25 @@ set -e
 source "/opt/ros/$ROS_DISTRO/setup.bash"
 source "/uros_ws/install/setup.bash"
 
-ros2 run micro_ros_agent micro_ros_agent serial --dev /dev/serial/by-id/usb-ZEPHYR_Zephyr_ST_Discovery_4E345008004C0049-if00 
+DEV=/dev/serial/by-id/usb-ZEPHYR_Zephyr_ST_Discovery_4E345008004C0049-if00 
+
+while true; do
+    while [ ! -e "$DEV" ]; do
+        sleep 1
+        echo "Waiting for device"
+    done
+
+    echo "Serial detected. Running agent."
+
+    ros2 run micro_ros_agent micro_ros_agent serial --dev $DEV -v6 &
+    export APP_PID=$!
+
+    while [ -e "$DEV" ]; do
+        sleep 1
+    done
+
+    echo "Serial disconnected. Running agent."
+
+    kill -9 $APP_PID
+done
+

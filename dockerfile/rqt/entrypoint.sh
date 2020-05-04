1c01df62400c3ff9d007f2c6546ed9d2e502c480
Pablo Garrido <pablogs9@gmail.com>
2020-05-04 11:29:23 +0200

    Minor fixes

--- dockerfile/rqt/entrypoint.sh
+++ dockerfile/rqt/entrypoint.sh
@@ -3,4 +3,10 @@ set -e
 
 source "/opt/ros/$ROS_DISTRO/setup.bash"
 
+while true; do 
+    if ros2 topic list | grep "sensors/imu" > /dev/null; then 
+        break
+    fi
+done
+
 exec rqt --perspective-file /src/demo_sensors.perspective
